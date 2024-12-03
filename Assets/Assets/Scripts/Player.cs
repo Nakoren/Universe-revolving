29d83c39f080e074753ea557710107350fdb72c9
Nakoren <minenkov_vladimir2003@inbox.ru>
2024-12-03 17:56:06 +0300

    Camera rotation to cursor (WIP)
    Added detection of point which pointed by cursor.

--- Assets/Assets/Scripts/Player.cs
+++ Assets/Assets/Scripts/Player.cs
@@ -1,19 +1,23 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[RequireComponent(typeof (CharacterController))]
+[RequireComponent(typeof(CharacterController))]
 
 public class Player : MonoBehaviour
 {
     public int speed;
     public InputActionAsset inputActions;
+    public TerrainCollider terrainCollider;
+
     private InputAction m_moveAction;
     private CharacterController m_charController;
+    private Transform m_transform;
 
     private void Awake()
     {
         m_moveAction = inputActions.FindAction("Player/Move");
         m_charController = GetComponent<CharacterController>();
+        m_transform = GetComponent<Transform>();
     }
 
     private void Start()
@@ -23,10 +27,12 @@ public class Player : MonoBehaviour
 
     void Update()
     {
+
+        RotateToCursor();
         Vector2 move = m_moveAction.ReadValue<Vector2>();
         if (move.magnitude > 0)
         {
-            Move(move, new Vector2(1,1));
+            Move(move, new Vector2(1, 1));
         }
         else
         {
@@ -45,5 +51,18 @@ public class Player : MonoBehaviour
         Vector3 moveDirV3 = new Vector3(x, 0f, y);
         //Debug.Log(moveDirV3);
         m_charController.SimpleMove(moveDirV3 * speed);
+    }
+
+    public void RotateToCursor()
+    {
+        Vector3 position;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit rayHit;
+
+        if (terrainCollider.Raycast(ray, out rayHit, 1000))
+        {
+            position = rayHit.point;
+            m_transform.Rotate(position);
+        }
     }
 }
