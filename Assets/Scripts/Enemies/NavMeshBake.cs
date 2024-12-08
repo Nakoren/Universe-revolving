a3035e9e9b0b6f72d9e8157a91f6ba6b3e4fd0e5
Виктория Фурса <112850977+fursatora@users.noreply.github.com>
2024-12-09 01:19:06 +0300

    EnemyController with sphere collider
    Enemy следует за Player до тех пор, пока Player находится в области collider Enemy

---
+++ Assets/Scripts/Enemies/NavMeshBake.cs
@@ -0,0 +1,16 @@
+using Unity.AI.Navigation;
+using UnityEngine;
+
+public class NavMeshBake : MonoBehaviour
+{
+    public NavMeshSurface m_Surface;
+    void Start()
+    {
+        m_Surface.BuildNavMesh();
+    }
+
+    void Update()
+    {
+        
+    }
+}
