0c9c817ad49ed96994cd67161392ce5408884128
Виктория Фурса <112850977+fursatora@users.noreply.github.com>
2024-12-17 14:51:22 +0300

    EnemyController By Camera
    1.Enemy movement towards the player while in camera view (EnemyControllerByCamera)
    2.Added enemy prefab and scene with enemies prefab

---
+++ Assets/Scripts/Enemies/EnemyControllerByCamera.cs
@@ -0,0 +1,65 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyControllerByCamera : MonoBehaviour
+{
+    private GameObject enemy;
+    private Collider enemyCollider;
+    private NavMeshAgent m_meshAgent;
+    private Transform m_target;
+
+    [SerializeField] private float speed = 2f;
+    [SerializeField] private Transform player;
+    [SerializeField] private Camera cam;
+    
+    Plane[] planes;
+
+     private void Awake()
+    {
+        m_meshAgent = GetComponent<NavMeshAgent>();
+        m_meshAgent.speed=speed;
+    }
+    void Start()
+    {
+        cam = Camera.main;
+        enemyCollider =  GetComponent<Collider>();
+    }
+    /* private void FixedUpdate()
+    {
+        if(m_target)
+        {
+            if(Time.frameCount % 3 == 0)
+            {
+                m_meshAgent.destination = m_target.position;
+            }
+        }
+
+        planes = GeometryUtility.CalculateFrustumPlanes(cam);
+        if (GeometryUtility.TestPlanesAABB(planes, objCollider.bounds))
+        {
+            m_target =null;
+            Debug.Log("Enemy has been detected");
+        }
+        else
+        {
+            m_target = player.transform;
+            m_meshAgent.speed = speed;
+            Debug.Log("Nothing has been detected");
+        }
+    }*/
+
+    void Update()
+    {
+        planes = GeometryUtility.CalculateFrustumPlanes(cam);
+        if (GeometryUtility.TestPlanesAABB(planes, enemyCollider.bounds))
+        {
+            m_meshAgent.destination = player.position;
+            Debug.Log("Enemy has been detected");
+        }
+        else
+        {
+            m_meshAgent.ResetPath();
+            Debug.Log("Nothing has been detected");
+        }
+    }
+}
