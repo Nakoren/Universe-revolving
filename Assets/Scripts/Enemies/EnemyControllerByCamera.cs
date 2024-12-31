8414f85394dd58b79a4474ab133225990d7caa12
Виктория Фурса <112850977+fursatora@users.noreply.github.com>
2025-01-01 02:57:58 +0300

    StopDistance
    Враг останавливается за расстояние вводимое в поле Stop Distance

--- Assets/Scripts/Enemies/EnemyControllerByCamera.cs
+++ Assets/Scripts/Enemies/EnemyControllerByCamera.cs
@@ -3,63 +3,49 @@ using UnityEngine.AI;
 
 public class EnemyControllerByCamera : MonoBehaviour
 {
-    private GameObject enemy;
-    private Collider enemyCollider;
     private NavMeshAgent m_meshAgent;
-    private Transform m_target;
+    private Collider enemyCollider;
 
     [SerializeField] private float speed = 2f;
+    [SerializeField] private float stopDistance = 2f; 
     [SerializeField] private Transform player;
     [SerializeField] private Camera cam;
-    
-    Plane[] planes;
 
-     private void Awake()
+    private Plane[] planes;
+
+    private void Awake()
     {
         m_meshAgent = GetComponent<NavMeshAgent>();
-        m_meshAgent.speed=speed;
+        m_meshAgent.speed = speed;
+        m_meshAgent.stoppingDistance = stopDistance; 
     }
-    void Start()
+
+    private void Start()
     {
         cam = Camera.main;
-        enemyCollider =  GetComponent<Collider>();
+        enemyCollider = GetComponent<Collider>();
     }
-    /* private void FixedUpdate()
-    {
-        if(m_target)
-        {
-            if(Time.frameCount % 3 == 0)
-            {
-                m_meshAgent.destination = m_target.position;
-            }
-        }
 
-        planes = GeometryUtility.CalculateFrustumPlanes(cam);
-        if (GeometryUtility.TestPlanesAABB(planes, objCollider.bounds))
-        {
-            m_target =null;
-            Debug.Log("Enemy has been detected");
-        }
-        else
-        {
-            m_target = player.transform;
-            m_meshAgent.speed = speed;
-            Debug.Log("Nothing has been detected");
-        }
-    }*/
-
-    void Update()
+    private void Update()
     {
         planes = GeometryUtility.CalculateFrustumPlanes(cam);
+
         if (GeometryUtility.TestPlanesAABB(planes, enemyCollider.bounds))
         {
-            m_meshAgent.destination = player.position;
-            Debug.Log("Enemy has been detected");
+            float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+            if (distanceToPlayer > stopDistance)
+            {
+                m_meshAgent.destination = player.position;
+            }
+            else
+            {
+                m_meshAgent.ResetPath();
+            }
         }
         else
         {
             m_meshAgent.ResetPath();
-            Debug.Log("Nothing has been detected");
         }
     }
 }
