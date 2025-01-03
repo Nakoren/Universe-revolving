c454f59badd52b721adc5f12179ba6e139d24f1f
Satoru-Gojo-Burger <grah228@yandex.ru>
2025-01-03 23:11:57 +0300

    Fix

--- Assets/Scripts/Weapon/SO/MagazineDataSO.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Magazine", menuName = "Magazine")]
-public class MagazineDataSO : ScriptableObject
-{
-    public int cage = 10;
-    public float recharge = 1f;
-    // rechargeEvent
-}
--- Assets/Scripts/Weapon/SO/ScopeDataSO.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Scope", menuName = "Scope")]
-public class ScopeDataSO : ScriptableObject
-{
-    public float spread = 1f;
-    public float Range = 15;
-    public float DamadeFactor2 = 1;
-}
