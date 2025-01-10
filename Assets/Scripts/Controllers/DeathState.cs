69c20bb3051b63750876d74fffb9de61c61b0af8
Nakoren <minenkov_vladimir2003@inbox.ru>
2025-01-10 15:11:46 +0300

    Death state

--- Assets/Scripts/Controllers/DeathState.cs
+++ Assets/Scripts/Controllers/DeathState.cs
@@ -1,7 +1,6 @@
-
 using UnityEngine;
 
-public class LevelController : MonoBehaviour
+public class DeathState : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
