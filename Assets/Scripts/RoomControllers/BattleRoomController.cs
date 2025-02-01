c8cdd5c8b6ceec9f3724f180990c2ddcdc463ddb
Виктория Фурса <112850977+fursatora@users.noreply.github.com>
2025-02-01 14:15:04 +0300

    Fix Enemy Death
    при смерти персонаж не скользит, а останавливается
    
    # Conflicts:
    #	Assets/Art/Enemies/Fantasy knight/Animations/Knight@Sword And Shield Death.fbx.meta
    #	Assets/Scripts/Enemies/Actions/AgentDeath.cs
    #	Assets/Scripts/Enemies/Actions/Attack/AgentMeleeAttack.cs

--- Assets/Scripts/RoomControllers/BattleRoomController.cs
+++ Assets/Scripts/RoomControllers/BattleRoomController.cs
@@ -1,16 +1,6 @@
 using UnityEngine;
 
-public class BattleRoomController : MonoBehaviour
+public class BattleRoomController : RoomController
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] Enemy
 }
