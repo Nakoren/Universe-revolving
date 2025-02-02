8fcd495a30e76441380f9251c3866f5232158faf
Nakoren <minenkov_vladimir2003@inbox.ru>
2025-02-02 23:07:45 +0300

    Room controller and prefab updates

--- Assets/Scripts/RoomControllers/RoomController.cs
+++ Assets/Scripts/RoomControllers/RoomController.cs
@@ -7,10 +7,13 @@ public class RoomController : MonoBehaviour
 {
     [SerializeField] public Transform startPosition;
 
-    [SerializeField] GameObject[] openTransitions;
+    [SerializeField] GameObject[] activeTransitions;
+    [SerializeField] GameObject[] inActiveTransitions;
     [SerializeField] GameObject[] closeTransitions;
 
     public Action<int> onRoomChange;
+    private int connectionsCount;
+    private bool instantCompletion = true;
 
     private Room[] connectedRooms;
     private void Awake()
@@ -20,17 +23,31 @@ public class RoomController : MonoBehaviour
 
     public void Initialize(List<Room> connectedRoomsList)
     {
-        for (int i = 0; i < connectedRoomsList.Count; ++i) {
-            openTransitions[i].SetActive(true);
-            Transitor curTransitor = openTransitions[i].GetComponent<Transitor>();
+        connectionsCount = connectedRoomsList.Count;
+        for (int i = 0; i < connectionsCount; ++i) {
+            Transitor curTransitor = activeTransitions[i].GetComponent<Transitor>();
             curTransitor.Initiate(connectedRoomsList[i], i);
             curTransitor.onActivate += OnRoomChange;
+
+            inActiveTransitions[i].SetActive(true);
         }
-        for (int i = connectedRoomsList.Count; i < closeTransitions.Length; i++)
+        for (int i = connectionsCount; i < closeTransitions.Length; i++)
         {
             closeTransitions[i].SetActive(true);
         }
-        //Debug.Log(startPosition);
+        if(instantCompletion)
+        {
+            FinishRoomTask();
+        }
+    }
+
+    private void FinishRoomTask()
+    {
+        for (int i = 0; i < connectionsCount; ++i)
+        {
+            activeTransitions[i].SetActive(false);
+            inActiveTransitions[i].SetActive(true);
+        }
     }
 
     public void OnRoomChange(int ind)
