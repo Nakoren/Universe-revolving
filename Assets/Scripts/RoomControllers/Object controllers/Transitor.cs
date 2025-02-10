d837c113dedc0b5acfc1ca15ed926e412732140b
Nakoren <minenkov_vladimir2003@inbox.ru>
2025-02-10 19:18:13 +0300

    Room update
    Fixes for town some rooms and update for scripts

--- Assets/Scripts/RoomControllers/Object controllers/Transitor.cs
+++ Assets/Scripts/RoomControllers/Object controllers/Transitor.cs
@@ -6,10 +6,21 @@ public class Transitor : IInteractable
     public int targetInd;
     public Room roomSO;
     public Action<int> onActivate;
+    private DoorsAnimationController doorController;
+    private bool isActive = false;
+    private void Awake()
+    {
+        doorController = GetComponent<DoorsAnimationController>();   
+    }
     public void Initiate(Room room, int roomInd)
     {
         roomSO = room;
         targetInd = roomInd;
+    }
+    public void Enable()
+    {
+        this.isActive = true;
+        doorController.OpenDoorsAnimation();
     }
 
     override public void Interact()
