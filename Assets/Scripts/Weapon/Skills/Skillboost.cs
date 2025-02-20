e2866c5408a4ac61976d53b0fcfdef37ecfaae4c
Satoru-Gojo-Burger <grah228@yandex.ru>
2025-02-21 02:28:47 +0300

    fix

--- Assets/Scripts/Weapon/Skills/Skillboost.cs
+++ Assets/Scripts/Weapon/Skills/Skillboost.cs
@@ -4,20 +4,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "boost", menuName = "Skills/boost")]
 public class Skillboost : ISkill
 {
-<<<<<<< HEAD
-    public bool m_isCooldown = false;
-    public int cooldown = 2; // Для иконок перезарядки
-    public void Awake()
-    {
-        m_isCooldown = false;
-    }
-    public override void OnSkillStart()
-    {
-        if (m_isCooldown == false)
-        {
-            m_isCooldown = true;
-            Debug.Log("Skillboost used");
-=======
     public void Awake()
     {
         m_isColdown = false;
@@ -28,7 +14,6 @@ public class Skillboost : ISkill
         {
             Debug.Log("Skillboost used");
             m_isColdown = true;
->>>>>>> 8e4c492236835aa814af70bff0ed799e2e86f7fc
             CoolldownDelay();
         }
         else 
@@ -43,11 +28,8 @@ public class Skillboost : ISkill
     private IEnumerator CoolldownDelay()
     {
         yield return new WaitForSeconds(cooldown);
-<<<<<<< HEAD
-        m_isCooldown = false;
-=======
         m_isColdown = false;
->>>>>>> 8e4c492236835aa814af70bff0ed799e2e86f7fc
+
     }
     private IEnumerator Delay()
     {
--- Assets/Scripts/Weapon/Skills/Skillhealing.cs
+++ Assets/Scripts/Weapon/Skills/Skillhealing.cs
@@ -4,22 +4,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "healing", menuName = "Skills/healing")]
 public class Skillhealing : ISkill
 {
-<<<<<<< HEAD
-    public bool m_isCooldown = false;
-    public int cooldown = 2; // Для иконок перезарядки
-    public void Awake()
-    {
-        m_isCooldown = false;
-    }
-    public override void OnSkillStart()
-    {
-        if (m_isCooldown == false)
-        {
-            m_isCooldown = true;
-            Debug.Log("Skillhealing used");
-            CoolldownDelay();
-            
-=======
+
     public void Awake()
     {
         m_isColdown = false;
@@ -31,7 +16,7 @@ public class Skillhealing : ISkill
             Debug.Log("Skillhealing used");
             m_isColdown = true;
             CoolldownDelay();
->>>>>>> 8e4c492236835aa814af70bff0ed799e2e86f7fc
+
         }
         else 
         {
@@ -44,14 +29,10 @@ public class Skillhealing : ISkill
     }
     private IEnumerator CoolldownDelay()
     {
-<<<<<<< HEAD
-        
-        yield return new WaitForSeconds(cooldown);
-        m_isCooldown = false;
-=======
+
         yield return new WaitForSeconds(cooldown);
         m_isColdown = false;
->>>>>>> 8e4c492236835aa814af70bff0ed799e2e86f7fc
+
     }
     private IEnumerator Delay()
     {
